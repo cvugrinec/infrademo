b2ff521a0f041ab57db347dcde9bef9df96f134b
chris vugrinec <chrisvugrinec@gmail.com>
2016-12-21 22:56:40 +0100

    added oms install agent

---
+++ scripts/installOmsAgent.sh
@@ -0,0 +1,3 @@
+# go to [your oms workspaceid ].portal.mms.microsoft.com
+# find your workspaceid (-w) and shared key (-s) ...int eh settings tab/ connected sources 
+sudo sh ./omsagent-1.2.0-148.universal.x64.sh --install -w [ YOUR OMS WORKSPACE ID ]  [ YOUr SHARED KEY ]
