c0cbd52c74992f671e09b77b7345bf5092bd9b29
chris vugrinec <chrisvugrinec@gmail.com>
2016-12-22 01:42:10 +0100

    fix

--- scripts/installOmsAgent.sh
+++ scripts/installOmsAgent.sh
@@ -1,5 +1,5 @@
 # go to [your oms workspaceid ].portal.mms.microsoft.com
 # find your workspaceid (-w) and shared key (-s) ...int eh settings tab/ connected sources 
-wget https://github.com/Microsoft/OMS-Agent-for-Linux/archive/OMSAgent-201611-v1.2.1-148.tar.gz
-tar -zxvf OMSAgent-201611-v1.2.1-148.tar.gz
+wget https://github.com/Microsoft/OMS-Agent-for-Linux/releases/download/OMSAgent-201610-v1.2.0-148/omsagent-1.2.0-148.universal.x64.sh
+cdmod 750 omsagent-1.2.0-148.universal.x64.sh
 sudo sh ./omsagent-1.2.0-148.universal.x64.sh --install -w [ YOUR OMS WORKSPACE ID ]  [ YOUr SHARED KEY ]
